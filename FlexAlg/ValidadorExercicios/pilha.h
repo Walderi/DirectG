5d1105ad6134c3503222b2601bf813a6c2c50136
gomesgeorgelucas <gomes.georgelucas@gmail.com>
2013-06-06 21:07:57 -0400

    [HEADER]

---
+++ FlexAlg/ValidadorExercicios/pilha.h
@@ -0,0 +1,17 @@
+#ifndef _PILHA_H_
+#define _PILHA_H_
+
+#define MAX_TAM 9999
+
+typedef int tipoLinha;
+
+typedef char [MAX_TAM] tipoString;
+
+typedef struct TipoPilha {
+	tipoLinha lineNo;
+	tipoString Errno;
+} TipoPilha;
+
+int push (TipoPilha pilha );
+
+int pop_all (TipoPilha pilha);
